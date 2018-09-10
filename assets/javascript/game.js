c2d896c40b94473b7392459857e1da53a00ba6c5
Alyssa Templeton <templetonalyssa@gmail.com>
2018-09-10 11:04:07 -0400

    Something on the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
 
-
-
-//Create a list of words
 var wordArray = ["skeleton","haunted","corpse","spiderweb", "despair", "macabre", "bloodless","agony"];
 var tempArray = [];
-var computerWord = "";
+var computerWord = document.getElementById("randomword");
 var guesses = 12;
+
 
 //ON THE PAGE (HOW IT LOOKS)
 //"Press any key to get started"
@@ -17,36 +15,40 @@ var guesses = 12;
 
 //WHAT THE PAGE DOES WHEN YOU START PRESSING THINGS
 //displays the dish dot dash of a random word
-//records the key pressed and displays it in the dish dot 
-//records the keys pressed 
+//records the key pressed and displays it in the dish dot
+//records the keys pressed
 //gives you a finite number of guesses
 
 function gameStart(){
 
-   guesses = 12;
-   tempArray = [];
-        
-  computerWord = wordArray[Math.floor(Math.random()*wordArray.length)];
- 
-   console.log(computerWord);
+  guesses = 12;
+  tempArray = [];
 
-   for (var i = 0; i<computerWord.length; i++) {
-   tempArray.push("_")  
-   }
-   console.log(tempArray);
+
+ computerWord = wordArray[Math.floor(Math.random()*wordArray.length)];
+
+  console.log(computerWord);
+
+  for (var i = 0; i<computerWord.length; i++) {
+  tempArray.push("_") ;
+
+  }
+  console.log(tempArray);
 
 }
 
-
 document.onkeyup = function(event){
 
-  console.log(event.key)
-  for (var i = 0; i<computerWord.length; i++) {
-    if (event.key === computerWord[i])  {
-      tempArray[i]=event.key
-    }
-    }
- console.log(tempArray);
+
+  document.getElementById("randomword").innerHTML = tempArray;
+
+ console.log(event.key)
+ for (var i = 0; i<computerWord.length; i++) {
+   if (event.key === computerWord[i])  {
+     tempArray[i]=event.key;
+   }
+   }
+console.log(tempArray);
 };
 
-gameStart ()
+gameStart()
