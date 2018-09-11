9e5864e43a5fe1ca5d816c6f061c472409b97e49
Alyssa Templeton <templetonalyssa@gmail.com>
2018-09-11 15:12:46 -0400

    with reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-
-var wordArray = ["skeleton","haunted","corpse","spiderweb", "despair", "macabre", "bloodless","agony"];
+var wordArray = ["skeleton", "haunted", "corpse", "spiderweb", "despair", "macabre", "bloodless", "agony"];
 var tempArray = [];
 var computerWord = document.getElementById("randomword");
-var guesses = 12;
+var maxGuesses = 10;
+var remainingGuesses = 0;
 
 
 //ON THE PAGE (HOW IT LOOKS)
@@ -19,36 +19,47 @@ var guesses = 12;
 //records the keys pressed
 //gives you a finite number of guesses
 
-function gameStart(){
-
-  guesses = 12;
-  tempArray = [];
 
 
- computerWord = wordArray[Math.floor(Math.random()*wordArray.length)];
+//what happens when you load the page
 
+function gameStart() {
+
+
+  computerWord = wordArray[Math.floor(Math.random() * wordArray.length)];
   console.log(computerWord);
 
-  for (var i = 0; i<computerWord.length; i++) {
-  tempArray.push("_") ;
-
+  for (var i = 0; i < computerWord.length; i++) {
+    tempArray.push("_");
   }
   console.log(tempArray);
 
+
 }
 
-document.onkeyup = function(event){
+//what happens when you press a key-the random word is indexed and shown on the display.
+//when the correct letter is guessed, it appears in the blank word.
+
+document.onkeyup = function (event) {
 
 
- console.log(event.key)
- for (var i = 0; i<computerWord.length; i++) {
-   if (event.key === computerWord[i])  {
-     tempArray[i]=event.key;
-   }
+  console.log(event.key)
+  for (var i = 0; i < computerWord.length; i++) {
+    if (event.key === computerWord[i]) {
+      tempArray[i] = event.key;
+    }
 
-   document.getElementById("randomword").innerHTML = tempArray;
-   }
-console.log(tempArray);
+    document.getElementById("randomword").innerHTML = tempArray;
+  }
+  console.log(tempArray);
 };
 
-gameStart()
+//the gameStart(); below is not a superfluous mistake, the page doesn't work when you remove it
+
+gameStart();
+
+//Cannot figure out how to restart the game after a certain number of tries, used a restart button instead
+
+function myFunction() {
+  location.reload();
+}
